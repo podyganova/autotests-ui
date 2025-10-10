622f1c28051c3714d43164ae6553dea97890db2a
podyganova <96391663+podyganova@users.noreply.github.com>
2025-10-10 21:43:04 +0300

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

---
+++ tests/test_create_course.py
@@ -0,0 +1,30 @@
+import pytest
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_empty_courses_list(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+
+    # Переходим на страницу создания Courses
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
+    # Создание Course
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form(title='', description='', estimated_time='', max_score='0', min_score='0')
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image("./testdata/files/image.png")
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.fill_create_course_form(title='Playwright', description='Playwright', estimated_time='2 weeks', max_score='100', min_score='10')
+    create_course_page.click_create_course_button()
+
+    # Проверка отображениея
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(index=0, title='Playwright', estimated_time='2 weeks', max_score='100', min_score='10')
