9f272dedf5b035e055af61f025176441eaf6c0cf
podyganova <96391663+podyganova@users.noreply.github.com>
2025-10-10 22:38:22 +0300

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,29 +1,43 @@
 import pytest
-from playwright.sync_api import expect, Page
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state: Page):  # Создаем тестовую функцию
+def test_empty_courses_list(courses_list_page: CoursesListPage):  # Создаем тестовую функцию
 
     # Переходим на страницу Courses
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
     # Проверяем, что на странице "Courses" отображается заголовок "Courses"
-    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_title).to_be_visible()
-    expect(courses_title).to_have_text("Courses")
+    courses_list_page.check_visible_courses_title()
 
     # Проверяем, что на странице "Courses" отображается текст блока "There is no results"
-    courses_results = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(courses_results).to_be_visible()
-    expect(courses_results).to_have_text("There is no results")
+    courses_list_page.check_visible_empty_view()
 
-    # Проверяем на странице "Courses" видимость иконки пустого блока
-    courses_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(courses_icon).to_be_visible()
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
 
-    # Проверяем, что на странице "Courses" отображается текст описания блока
-    courses_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(courses_description).to_be_visible()
-    expect(courses_description).to_have_text("Results from the load test pipeline will be displayed here")
+    # Переходим на страницу создания Courses
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
+    # Создание Course
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form(title='', description='', estimated_time='', max_score='0', min_score='0')
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image("./testdata/files/image.png")
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.fill_create_course_form(title='Playwright', description='Playwright', estimated_time='2 weeks', max_score='100', min_score='10')
+    create_course_page.click_create_course_button()
+
+    # Проверка отображениея
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(index=0, title='Playwright', estimated_time='2 weeks', max_score='100', min_score='10')
