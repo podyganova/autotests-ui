24926b69ce9fc266a48ac71b89f89738bea99067
podyganova <96391663+podyganova@users.noreply.github.com>
2025-10-12 01:16:08 +0300

    Интеграция Allure в автотесты. Практическая работа с Allure аннотациями

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -1,17 +1,33 @@
 import pytest
+import allure
+from allure_commons.types import Severity
 from pages.authentication.login_page import LoginPage
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStory.AUTHORIZATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.AUTHORIZATION)
 class TestAuthorization:
     @pytest.mark.parametrize("email, password", [
         ("user.name@gmail.com", "password"),
         ("user.name@gmail.com", "  "),
         ("  ", "password")
     ])
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.title("User login with wrong email or password")
+    @allure.severity(Severity.CRITICAL)
     def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
         login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
@@ -22,6 +38,9 @@ class TestAuthorization:
         # Проверяем наличие сообщения об ошибке
         login_page.check_visible_wrong_email_or_password_alert()
 
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.title("User login with correct email and password")
+    @allure.severity(Severity.BLOCKER)
     def test_successful_authorization(
             self, login_page: LoginPage, dashboard_page: DashboardPage, registration_page: RegistrationPage):
         # Переход на страницу регистрации
