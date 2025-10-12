e4c05d89acc5a8884d8956abff6606a4278f913d
podyganova <96391663+podyganova@users.noreply.github.com>
2025-10-12 23:47:55 +0300

    Запуск автотестов на разных браузерах. Практическое задание: запуск автотестов на нескольких браузерах

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -8,9 +8,9 @@ from config import settings
 from tools.routes import AppRoute
 
 
-@pytest.fixture
+@pytest.fixture(params=settings.browsers)  # Добавляем параметризацию
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+    yield from initialize_playwright_page(playwright, test_name=request.node.name, browser_type=request.param)
 
 
 @pytest.fixture(scope="session")
@@ -32,11 +32,12 @@ def initialize_browser_state(playwright: Playwright):
     browser.close()
 
 
-@pytest.fixture
+@pytest.fixture(params=settings.browsers)
 def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state=settings.browser_state_file  # Используем settings.browser_state_file
+        browser_type=request.param,
+        storage_state=settings.browser_state_file
     )
 
--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -1,16 +1,17 @@
 import allure
 from playwright.sync_api import Playwright, Page
 
-from config import settings
+from config import settings, Browser
 
 
 def initialize_playwright_page(
         playwright: Playwright,
         test_name: str,
+        browser_type: Browser,
         storage_state: str | None = None
 ) -> Page:
-    # Используем settings.headless
-    browser = playwright.chromium.launch(headless=settings.headless)
+
+    browser = playwright[browser_type].launch(headless=settings.headless)
     # Используем settings.videos_dir
     context = browser.new_context(
         base_url=settings.get_base_url(), storage_state=storage_state, record_video_dir=settings.videos_dir)
