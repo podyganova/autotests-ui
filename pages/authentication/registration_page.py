f60bd21d7f581f926946d065e3ac71836a218f19
podyganova <podyganova@users.noreply.github.com>
2025-10-13 20:00:35 +0000

    deploy: c9156d51642828d8d9aee501bdc671a33cbb1e45

--- pages/authentication/registration_page.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import Page
-from pages.base_page import BasePage
-from components.authentication.registration_form_component import RegistrationFormComponent
-
-
-class RegistrationPage(BasePage):
-    def __init__(self, page: Page):
-        super().__init__(page)
-        self.registration_form_component = RegistrationFormComponent(page)
