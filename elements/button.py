074aabfadf483352b7628183a1ad8f65ac168849
podyganova <96391663+podyganova@users.noreply.github.com>
2025-10-14 19:58:36 +0300

    Практикуемся в интеграции покрытия в UI тесты. Практическое задание: Работа с инструментом ui-coverage-tool

--- elements/button.py
+++ elements/button.py
@@ -1,5 +1,6 @@
 import allure
 from playwright.sync_api import expect
+from ui_coverage_tool import ActionType
 
 from elements.base_element import BaseElement
 from tools.logger import get_logger
@@ -18,6 +19,7 @@ class Button(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_enabled()
+        self.track_coverage(ActionType.ENABLED, nth, **kwargs)
 
     def check_disabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is disabled'
@@ -25,4 +27,4 @@ class Button(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_disabled()
-
+        self.track_coverage(ActionType.DISABLED, nth, **kwargs)
--- elements/textarea.py
+++ elements/textarea.py
@@ -1,5 +1,6 @@
 import allure
 from playwright.sync_api import Locator, expect
+from ui_coverage_tool import ActionType
 
 from elements.base_element import BaseElement
 from tools.logger import get_logger
@@ -15,12 +16,16 @@ class Textarea(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('textarea').first
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> Locator:
+        return super().get_locator(nth, **kwargs).locator('textarea').first
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             locator.fill(value)
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
 
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
@@ -28,3 +33,4 @@ class Textarea(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
